e87e73c5619fd146ed06c8931e31fdf2eb02d1d3
Hui Yee Leong <66815986+huiyeeleong@users.noreply.github.com>
2021-09-27 21:59:37 +1000

    updated script lambda function

--- hui-simple-app/api/get-photos/index.ts
+++ hui-simple-app/api/get-photos/index.ts
@@ -5,18 +5,39 @@ import {
     APIGatewayProxyResultV2
 } from 'aws-lambda';
 
+//linking our s3 bucket to our lambda function
+import {S3} from 'aws-sdk';
+import {Bucket} from '@aws-cdk/aws-s3';
+ 
+const s3 = new S3();
+
 //import bucket name
-const bucketName = process.env.PHOTO_BUCKET_NAME;
+const bucketName = process.env.PHOTO_BUCKET_NAME!;
+
+function generateUrl(asset){}
 
 //create function
 async function getPhotos(
-    event: APIGatewayProxyEventV2, 
-    context: Context): Promise <APIGatewayProxyResultV2> {
-        console.log("I got the bucket name and it is " + bucketName);
-        return {
-            statusCode: 200,
-            body: 'Hello from lambda, it is working!',
-        };
-
-}
+    event: APIGatewayProxyEventV2,
+    context: Context
+  ): Promise<APIGatewayProxyResultV2> {
+    console.log('I got the bucket name and it is ' + bucketName);
+    try {
+      const { Contents: results } = await s3
+        .listObjects({ Bucket: bucketName })
+        .promise();
+      const photos = await Promise.all(
+        results!.map((result) => generateUrl(result))
+      );
+      return {
+        statusCode: 200,
+        body: JSON.stringify(photos),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: error.message,
+      };
+    }
+  }
 export {getPhotos}
