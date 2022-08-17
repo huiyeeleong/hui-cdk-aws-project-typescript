02c652cbd08da46759bec366f6b00d72396938ec
Leong <hui.yee.leong@accenture.com>
2022-08-17 13:30:37 +1000

    Updated new website

--- hui-simple-app/frontend/src/App.tsx
+++ hui-simple-app/frontend/src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {Carousel} from 'react-boostrap';
+import 'bootstrap/dist/css/boostrap.min.css'
 
 function App() {
+  const [allPhotos, setAllPhotos] = useState([]);
+  async function fetchPhotos() {
+    const data = await axios.get(`${baseUri}getAllPhotos`);
+    setAllPhotos(data);
+  }
+  useEffect(() => {
+    fetchPhotos()
+  }, []);
+
+
+  const baseUri: string = process.env.REACT_APP_API_URL!;
+  console.log(baseUri);
+
+  function getCarouselImage(photo: any){
+    return (
+    <Carousel.Item interval={1000} style={{height: 350}}>
+      <img src={photo.url} alt={photo.filename} className='h-100' />
+      <Carousel.Caption>
+        <h3 style={{backgroundColor: 'rgba(0,0,0,.3)'}}>{photo.filename}</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello from react from CDK! (almost there)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App bg-secondary min-vh-100">
+    <h1 className='display-3 p-3 mb-5'>Super Mario and friends</h1>
+     <Carousel>{allPhotos.map(photo => {getCarouselImage(photo)})}</Carousel>
     </div>
   );
 }
